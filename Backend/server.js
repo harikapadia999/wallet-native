9b5f216444e7fdbd164a4f59fb2a769e28ba8031
harikapadia999 <harikapadia999@gmail.com>
2025-09-02 15:05:41 +0530

    feat :: Implement CRUD operations for transactions API

--- Backend/server.js
+++ Backend/server.js
@@ -5,6 +5,7 @@ import { sql } from "./config/db.js";
 dotenv.config();
 
 const app = express();
+app.use(express.json());
 const PORT = process.env.PORT || 5001;
 
 async function initDB() {
@@ -23,8 +24,60 @@ async function initDB() {
     process.exit(1);
   }
 }
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+
+app.get("/api/transactions/:user_id", async (req, res) => {
+  try {
+    const { user_id } = req.params;
+    if (!user_id) {
+      return res.status(400).json({ message: "User ID is required." });
+    }
+
+    const transactions = await sql`
+      SELECT * FROM transactions WHERE user_id = ${user_id} ORDER BY date DESC
+    `;
+    res.status(200).json(transactions);
+  } catch (error) {
+    console.log("Error fetching transactions:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+app.post("/api/transactions", async (req, res) => {
+  try {
+    const { user_id, title, amount, category } = req.body;
+    if (!user_id || !title || amount === undefined || !category) {
+      return res.status(400).json({ error: "All fields are required." });
+    }
+    const transactions = await sql`
+      INSERT INTO transactions (user_id, title, amount, category) 
+      VALUES (${user_id}, ${title}, ${amount}, ${category}) 
+      RETURNING *`;
+
+    console.log(transactions);
+    res.status(201).json(transactions[0]);
+  } catch (error) {
+    console.log("Error creating transaction:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+app.delete("/api/transactions/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    if (!id) {
+      return res.status(400).json({ error: "Transaction ID is required." });
+    }
+    if (isNaN(parseInt(id))) {
+      return res.status(400).json({ message: "Invalid transaction ID." });
+    }
+    const result =
+      await sql`DELETE FROM transactions WHERE id = ${id} RETURNING *`;
+    if (result.length === 0) {
+      return res.status(404).json({ error: "Transaction not found." });
+    }
+    res.status(200).json({ message: "Transaction deleted successfully." });
+  } catch (error) {
+    console.log("Error deleting transaction:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 });
 
 initDB().then(() => {
